a404aad51e3e90862c9014b2ce8d2ff8acd76351
agoodis <agoodis@gmail.com>
2018-05-11 07:18:09 +0300

    First

--- TableViewApp/ViewController.swift
+++ TableViewApp/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let restarauntNames = ["Ogonek", "Banasi", "Milk", "Cow", "Kebab", "Marusia", "Classic", "Ben&Bon", "KidlLands", "Momy", "Clark's", "Level UP", "Sons", "Beef", "Beer House", "Nuga"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restarauntNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
 
-
+        cell?.textLabel?.text = restarauntNames[indexPath.row]
+        
+        return cell!
+    }
+    
 }
 
